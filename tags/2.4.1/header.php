607f8442070557eaf3ab9c551600198e4a88926d
MikelP <MikelP@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-09 17:11:32 +0000

    Checking in branches
    git-svn-id: https://plugins.svn.wordpress.org/wp-internal-links-lite@1064068 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/2.4.1/header.php
@@ -0,0 +1,28 @@
+<link rel="stylesheet" type="text/css" media="all" href="<?php echo INLPLN1 ?>css/style.css" />
+<div class="inlheader"  align="center" style="float: left;">
+<div align="center" style="float: left;">
+<img src="<?php echo INLPLN1?>images/Hub.png" border="0"/><br /><strong >Hub</strong> </div>
+<div align="center" style="float: left;">
+<img src="<?php echo INLPLN1?>images/Ring.png" border="0"/><br /><strong>Ring</strong> </div> 
+<div align="center" style="float: left;">
+<img src="<?php echo INLPLN1?>images/Web.png" border="0"/><br /><strong>Web</strong> </div>  
+<div align="center" style="float: left;">
+<img src="<?php echo INLPLN1?>images/Star.png" border="0"/><br /><strong>Star</strong> </div>  
+<div align="center" style="float: left;">
+<img src="<?php echo INLPLN1?>images/Legend.png" border="0"/><br /><strong>Legend</strong> </div>  
+
+</div>
+<div style="clear: both !important;"></div>
+
+<div class="promoheader"   style="float: left;">
+
+This is a free lite version of the full-fledged <a href="<?php echo getinlAffiliateURL()?>" target="_blank">No Sweat WP Internal Links</a> plugin. As such, it has certain limitations:<br />
+<ul style="padding-left:5px ;">
+<ol>It only allows you to create one of the four types of structures (the Ring).</ol>
+<ol>You can create or delete Rings, but you cannot edit them.</ol>
+</ul>
+Learn about all the possibilities the pro version of the plugin offers, and unleash all of its power by upgrading to a pro license in our website at <a href="<?php echo getinlAffiliateURL()?>" target="_blank">nosweatplugins.com.</a>
+
+</div>
+
+<div style="clear: both !important;"></div>
